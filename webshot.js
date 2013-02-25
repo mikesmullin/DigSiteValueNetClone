a616fbdc280e0a28eb6762b0983b95aac4b1b8e1
Mike Smullin <mike@smullindesign.com>
2013-02-24 20:59:59 -0700

    remove unused code

--- webshot.js
+++
@@ -1,6 +0,0 @@
-var webshot = require('webshot');
-
-webshot('http://www.google.com/', __dirname+'/static/public/user/google.png', function(err) {
-  if (err) { console.log(err); }
-  console.log('screenshot saved.');
-});
